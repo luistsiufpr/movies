7b3405ccdf5908e0ee5e5eee63a467862ebc7f99
Luis Lambranho <luistsiufpr@gmail.com>
2022-03-14 12:29:34 +0000

    issues with navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,11 @@ import {
 } from "react-router-dom";
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
-import Navbar from "components/NavBar";
+//import Navbar from "components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Listing />} />
         <Route path="/form">
